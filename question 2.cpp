026f31f9112195b28fbcea7d03147a355ed9efb4
Artem138 <t.k.200516@gmail.com>
2024-06-11 23:05:24 +0300

    Create question 2.cpp

---
+++ question 2.cpp
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants {
+
+    // Используйте inline constexpr, если ваш компилятор поддерживает C++17
+    inline constexpr int max_class_size = 30; 
+
+    // В противном случае используйте обычные constexpr
+    // constexpr int max_class_size = 30; 
+}
+
+#endif
